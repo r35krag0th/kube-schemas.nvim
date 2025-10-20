fe9e07564d7e79ca123ea050e4f5ffc842ccdf98
Bob Saska <git@r35.net>
2025-10-19 19:33:27 -0500

    feat: adding in a search subcommand

--- plugin/kube-schemas.lua
+++ plugin/kube-schemas.lua
@@ -7,9 +7,42 @@ if not config.options then
 	kube_schemas.setup()
 end
 
--- Create the main command
-vim.api.nvim_create_user_command("KubeSchemas", function()
-	kube_schemas.perform_selection()
+-- Subcommand handlers
+local subcommands = {
+	search = function()
+		kube_schemas.perform_selection()
+	end,
+}
+
+-- Create the main command with subcommand support
+vim.api.nvim_create_user_command("KubeSchemas", function(opts)
+	local subcommand = opts.fargs[1]
+
+	if not subcommand then
+		vim.notify("KubeSchemas requires a subcommand. Available: " .. table.concat(vim.tbl_keys(subcommands), ", "), vim.log.levels.ERROR)
+		return
+	end
+
+	local handler = subcommands[subcommand]
+	if handler then
+		handler()
+	else
+		vim.notify("Unknown subcommand: " .. subcommand, vim.log.levels.ERROR)
+		vim.notify("Available subcommands: " .. table.concat(vim.tbl_keys(subcommands), ", "), vim.log.levels.INFO)
+	end
 end, {
-	desc = "Select and insert a Kubernetes YAML schema modeline",
+	nargs = "+",
+	complete = function(arg_lead, cmdline, _)
+		local parts = vim.split(cmdline, "%s+")
+
+		-- Complete subcommand names
+		if #parts <= 2 then
+			return vim.tbl_filter(function(cmd)
+				return cmd:find(arg_lead, 1, true) == 1
+			end, vim.tbl_keys(subcommands))
+		end
+
+		return {}
+	end,
+	desc = "Kubernetes schema utilities",
 })
